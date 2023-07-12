5f599d9f0292040a51c039ada6da61764bc22735
Ana Grisham <anaunique@icloud.com>
2023-07-12 14:45:11 -0700

    completed thought route

--- routes/api/thoughtRoute.js
+++ routes/api/thoughtRoute.js
@@ -1,15 +1,30 @@
-// GET to get all thoughts
+const router = require('express').Router();
 
-// GET to get a single thought by its _id
+const{
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    removeReaction
+} = require ('../../controllers/thought-controller');
 
-// POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
+router
+    .route('/')
+    .get(getAllThoughts)
+    .post(createThought);
 
-// PUT to update a thought by its _id
+router
+    .route('/:id')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
 
-// DELETE to remove a thought by its _id
+router
+    .route('/:thoughtId/reactions').post(addReaction);
 
-// /api/thoughts/:thoughtId/reactions
+router
+    .route('/:thoughtId/reactions/:reactionId').delete(removeThought)
 
-// POST to create a reaction stored in a single thought's reactions array field
-
-// DELETE to pull and remove a reaction by the reaction's reactionId value
+    module.exports = router;
