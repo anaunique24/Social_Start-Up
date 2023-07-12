7e9d23b9068805ea7e3175f562e6534b8a153194
Ana Grisham <anaunique@icloud.com>
2023-07-12 13:51:43 -0700

    added comments to server file

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
+//requiring npm express and modularized files
 const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+//using middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
+//initializing the server
 db.once('open', () => {
     app.listen(PORT, () => console.log(`Now listening http://localhost:${PORT}/`));
 });
