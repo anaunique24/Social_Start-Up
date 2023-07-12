64dc2650be07545c6a45872ca80d5b3ee55afbdb
Ana Grisham <anaunique@icloud.com>
2023-07-12 13:36:54 -0700

    psuedo coded routes

---
+++ routes/api/thoughtRoute.js
@@ -0,0 +1,15 @@
+// GET to get all thoughts
+
+// GET to get a single thought by its _id
+
+// POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
+
+// PUT to update a thought by its _id
+
+// DELETE to remove a thought by its _id
+
+// /api/thoughts/:thoughtId/reactions
+
+// POST to create a reaction stored in a single thought's reactions array field
+
+// DELETE to pull and remove a reaction by the reaction's reactionId value
---
+++ routes/api/userRoute.js
@@ -0,0 +1,15 @@
+// GET all users
+
+// GET a single user by its _id and populated thought and friend data
+
+// POST a new user
+
+// PUT to update a user by its _id
+
+// DELETE to remove user by its _id
+
+// /api/users/:userId/friends/:friendId
+
+// POST to add a new friend to a user's friend list
+
+// DELETE to remove a friend from a user's friend list
