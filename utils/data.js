70059746cc96e3b04b7ce28c29370440c60a5f2f
Ana Grisham <anaunique@icloud.com>
2023-07-13 15:14:05 -0700

    created dumby data

---
+++ utils/data.js
@@ -0,0 +1,27 @@
+const usernames = [
+    'user1',
+    'user2',
+    'user3',
+    'user4',
+    'user5',
+    'user6',
+    'user7',
+    'user8',
+    'user9',
+    'user10',
+  ];
+  
+  const emails = [
+    'user1@example.com',
+    'user2@example.com',
+    'user3@example.com',
+    'user4@example.com',
+    'user5@example.com',
+    'user6@example.com',
+    'user7@example.com',
+    'user8@example.com',
+    'user9@example.com',
+    'user10@example.com',
+  ];
+  
+  module.exports = { usernames, emails };
